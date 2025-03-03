d7ba668d6a3f61ab5a79738e931b4f50de22f124
Chris Combs <ccombs@gmail.com>
2025-03-02 20:38:15 -0500

    3.3.2

--- build/jekyll_lunr_js_search.rb
+++ build/jekyll_lunr_js_search.rb
@@ -247,6 +247,6 @@ module Jekyll
 end
 module Jekyll
   module LunrJsSearch
-    VERSION = "3.3.1"
+    VERSION = "3.3.2"
   end
 end
--- lib/jekyll_lunr_js_search/version.rb
+++ lib/jekyll_lunr_js_search/version.rb
@@ -1,5 +1,5 @@
 module Jekyll
   module LunrJsSearch
-    VERSION = "3.3.1"
+    VERSION = "3.3.2"
   end
 end
